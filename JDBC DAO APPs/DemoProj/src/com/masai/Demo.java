6b3ae5442ec78cae779a4317093cf12eddf70d71
SubhadeepBid <subhadipbid@gmail.com>
2022-12-21 21:52:58 +0530

    How To Add Color To Console

--- JDBC DAO APPs/DemoProj/src/com/masai/Demo.java
+++ JDBC DAO APPs/DemoProj/src/com/masai/Demo.java
@@ -3,5 +3,11 @@ package com.masai;
 public class Demo {
 	public static void main(String[] args) {
 		System.out.println("First File On Unit 6...");
+		System.out.println(ConsoleColors.BLUE + ConsoleColors.WHITE_BACKGROUND + "+---------------------------+" + "\n"
+				   + "| 1. Login As Administrator |" + "\n"
+				   + "| 2. Login As Customer      |" + "\n"
+				   + "| 3. Exit                   |" + "\n"
+				   + "+---------------------------+" + ConsoleColors.RESET);
+		System.out.println("Helle Everyone");
 	}
 }
